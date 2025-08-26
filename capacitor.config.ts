444aff95a6c3c4fc1793504a2806ee71b2a6ccf4
Eduardo Zyger <47608839+Ezyger@users.noreply.github.com>
2025-08-26 17:12:03 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ezyger.treino.app',
+  appName: 'meu-app',
+  webDir: 'www'
+};
+
+export default config;
